ad972ce9a07aeaf0583f089b762ec2131ac014a3
JosPerez <jos.perez.mtz@gmail.com>
2022-03-01 02:51:01 -0600

    Agrega Pod auxiliar
    - Se implementa logíca para manejador de red

--- MovieApp/MovieApp/BaseController/BaseController.swift
+++ MovieApp/MovieApp/BaseController/BaseController.swift
@@ -5,21 +5,28 @@
 //  Created by jose perez on 28/02/22.
 //
 import UIKit
+import BackServices
 class BaseController: UIViewController {
-
+    /// Placeholder de  conectividad
+    let networkplacehoder: UIViewController = MANetworkPlaceholderVC()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        BSNetworkManager.shared.networkDelegate = self
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if true {
-            showNetworkPlaceholder()
-        }
+        BSNetworkManager.shared.start()
+        networkStatusPlaceholder(status: BSNetworkManager.shared.networkStatus())
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        BSNetworkManager.shared.cancel()
+    }
+}
+/// Extensión con la información de red
+extension BaseController: BSNetworkManagerDelegate {
     /// Muestra el placeholder de falta de conexión
     private func showNetworkPlaceholder() {
-        let networkplacehoder: UIViewController = MANetworkPlaceholderVC()
         addChild(networkplacehoder)
         networkplacehoder.view.frame = view.frame
         view.addSubview(networkplacehoder.view)
@@ -28,7 +35,16 @@ class BaseController: UIViewController {
     /// Remueve  el placeholder de falta de conexión
     private func removeNetworkPlaceholder() {
         willMove(toParent: nil)
-        view.removeFromSuperview()
-        removeFromParent()
+        networkplacehoder.view.removeFromSuperview()
+        networkplacehoder.removeFromParent()
+    }
+    /// Muestra u ocualta el placeholder  de conectividad
+    private func networkStatusPlaceholder(status: Bool) {
+        DispatchQueue.main.async {
+            !status ? self.showNetworkPlaceholder() : self.removeNetworkPlaceholder()
+        }
+    }
+    func didNetworkChange(status: Bool) {
+        networkStatusPlaceholder(status: status)
     }
 }
